5fcde73dc8b208834171e0f6a87e39ea6130bb05
John Phillips <johnphillips.dev@gmail.com>
2021-12-13 23:26:10 +0000

    preview images moved into service

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -4,7 +4,18 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ProductService {
+  previewImages = [
+    '../../assets/image-product-1.jpg',
+    '../../assets/image-product-2.jpg',
+    '../../assets/image-product-3.jpg',
+    '../../assets/image-product-4.jpg',
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  getPreviewImages() {
+    return this.previewImages.slice();
+  }
 }
