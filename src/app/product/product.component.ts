65144fc7f6c86d948eee39d510b85fdb453c8594
John Phillips <johnphillips.dev@gmail.com>
2021-12-13 14:54:18 +0000

    Added interactivity for mobile and tablet.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
+  index = 1;
+  preview = 0;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  carouselSwitch(isNext: boolean) {
+    if (this.index > 3 && isNext) {
+      this.index = 0;
+    } else if (this.index < 2 && !isNext) {
+      this.index = 4;
+    }
+
+    if (isNext) {
+      this.index++;
+    } else {
+      this.index--;
+    }
+  }
+
+  setCurrentImage(index: number) {
+    this.index = index;
+  }
 }
